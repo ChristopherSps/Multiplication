7825185d7e7dc42ca22ac6f62460f273693ec9d8
ChristopherSps <98600618+ChristopherSps@users.noreply.github.com>
2022-04-12 14:19:09 -0400

    Broken Tables.js

--- tables.js
+++ tables.js
@@ -1,16 +1,15 @@
 /* This is where we print out the times tables */
 
-var inter = document.createElement("inter");
-inner.setAttribute("id","inter");
-document.body.appendChild(inter);
-
-function tables(factor){
-    if(factor === "undefined"){
-        factor = prompt("Times tables for ");
+function tables(){
+    let factor = localStorage.getItem("problemFactor");
+    // if (factor === "undefined") {
+    //     alert("hey");
+    //     factor = prompt("Times tables for ");
+    // }
+    let tableText = "Table for "+factor+ "\n";
+    for (let row = min; row <= max; row++) {
+        tableText += factor+ " * " +row+ " = "+ factor*row + "\n";
+        alert(tableText);
     }
-    let table = "Table for "+factor+ "\n";
-    for(let row = min; row <= max; row++){
-        table += factor+ " * " +row+ " = "+ factor*row + "\n";
-    }
-    alert(table);
+    alert(tableText);
 }
